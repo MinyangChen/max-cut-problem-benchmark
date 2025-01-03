38ffa73704fc6287d43cfc7c6345e9a3e789bbdf
MinyangChen <1223605455@qq.com>
2025-01-03 16:36:47 +0000

    Generate the adjacency matrix for G51 problem.

--- generate_graph.py
+++ generate_graph.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import glob
 import re
@@ -7,17 +8,17 @@ def atoi(text):
     return int(text) if text.isdigit() else text
 
 def natural_keys(text):
-    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+    return [atoi(c) for c in re.split(r'(\d+)', text)]
 
 # Get the file path
-all_file =sorted(glob.glob("G*/G*.txt"), key=natural_keys)
+all_file = sorted(glob.glob("G*/G*.txt"), key=natural_keys)
 
 # Generate graph from file
 for i in range(len(all_file)):
     line_ct = 0
     with open(all_file[i]) as f:
         for line in f:
-            if line_ct == 0: # Get graph size
+            if line_ct == 0:  # Get graph size
                 graph = np.zeros((int(line.split()[0]), int(line.split()[0])), dtype=int)
             else:
                 idx_i = int(line.split()[0]) - 1
@@ -26,7 +27,10 @@ for i in range(len(all_file)):
                 graph[idx_i, idx_j] = weight
             line_ct += 1
 
+    # Generate output file name (e.g., G51_matrix.txt)
+    output_file_name = re.sub(r"\.txt$", "_matrix.txt", all_file[i])
+    output_file_name = re.sub(r".*/", "", output_file_name)  # Remove directories if necessary
+
     # Save graph
-    dir_name = re.sub(".txt", "", all_file[i])
-    dir_name = re.sub(".*/", "", dir_name)
-    np.savetxt(dir_name + "/matrix.txt", graph, fmt="%d")
+    np.savetxt(output_file_name, graph, fmt="%d")
+    print(f"Graph saved to: {output_file_name}")
